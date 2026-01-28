9090f549dcdeeffa1f530331534b966bfd2daf96
Neha More <nehmoreofficialmail@gmail.com>
2026-01-28 19:02:08 +0530

    create sign-up api route

--- api/index.js
+++ api/index.js
@@ -2,14 +2,18 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import router from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
 console.log('MangoDb is connected');
 }).catch((err)=>{
     console.log(err);
 });
+console.log("MONGO URL:", process.env.MONGO);
 const app=express();
+app.use(express.json());
 app.listen(3000,()=>{
     console.log('server is runnig on port 3000');
 });
 app.use('/api/user',router);
+app.use('/api/auth',authRoutes)
